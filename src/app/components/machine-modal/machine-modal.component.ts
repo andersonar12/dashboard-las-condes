3357268f1954bf072b8529494638748de80ebace
andersonar12 <aromerocangri.14@gmail.com>
2021-12-14 14:28:28 -0400

    2da seccion maquetada completamente

--- src/app/components/machine-modal/machine-modal.component.ts
+++ src/app/components/machine-modal/machine-modal.component.ts
@@ -3,6 +3,7 @@ import {MatDialog, MAT_DIALOG_DATA,MatDialogRef} from '@angular/material/dialog'
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-machine-modal',
   templateUrl: './machine-modal.component.html',
@@ -44,5 +45,45 @@ export class MachineModalComponent implements OnInit {
   /* ngAfterViewInit() {
     this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0 );
   } */
+
+  exportData(){
+
+    //Exporta a Excel
+    const props:any = {
+      'date':'Fecha',
+      'time':'Hora', 
+      'route':'Ruta', 
+      'bus_stop':'Paradero', 
+      'passengers':'Cant. de pasajeros'
+    }
+ 
+
+    const dataForXLSX = this.dataSource.data.map((itemObject)=>{
+
+      let object:any = {}
+
+      for (const property in itemObject) {
+
+        if (props.hasOwnProperty(property)) {
+
+          object[props[property]] = itemObject[property]
+
+        } else {
+
+          object[property] = itemObject[property]
+
+        }
+
+      }
+      
+      return object
+    })
+    console.log(dataForXLSX);
+
+    const workSheet = XLSX.utils.json_to_sheet(dataForXLSX, { header: ['Fecha','Hora','Ruta','Paradero', 'Cant. de pasajeros'] })
+    const workBook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workBook, workSheet, 'Maquina');
+    XLSX.writeFile(workBook, 'report.xlsx');
+  }
 
 }
--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -6,7 +6,7 @@ import * as echarts from 'echarts';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
@@ -65,6 +65,40 @@ export class StatisticsComponent implements OnInit {
 
   hoverFilters(personalizado:HTMLElement){
     personalizado.classList.add('hover-filters')
+  }
+
+  exportData(){
+
+    //Exporta a Excel
+    const props:any = {
+      'time':'Hora de Salida', 
+      'machine':'Nro. Máquina / Patente', 
+      'route':'Ruta', 
+    }
+
+    const dataForXLSX = this.dataSource.data.map((itemObject)=>{
+
+      let object:any = {}
+      for (const property in itemObject) {
+
+        if (props.hasOwnProperty(property)) {
+
+          object[props[property]] = itemObject[property]
+
+        } else {
+          object[property] = itemObject[property]
+
+        }
+      }
+      
+      return object
+    })
+    console.log(dataForXLSX);
+
+    const workSheet = XLSX.utils.json_to_sheet(dataForXLSX, { header: ['Hora de Salida','Nro. Máquina / Patente','Ruta'] })
+    const workBook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workBook, workSheet, 'Maquinas');
+    XLSX.writeFile(workBook, 'report.xlsx');
   }
 
 }
